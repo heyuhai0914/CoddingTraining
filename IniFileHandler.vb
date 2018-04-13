72088ead6f2f4db054a8204757488b98edbd2cc8
heyuhai0914 <38352921+heyuhai0914@users.noreply.github.com>
2018-04-13 23:22:16 +0900

    Add files via upload

---
+++ IniFileHandler.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Runtime.InteropServices
+
+Friend Class IniFileHandler
+    Declare Function GetPrivateProfileString Lib "KERNEL32.DLL" Alias "GetPrivateProfileStringA" (
+      <MarshalAs(UnmanagedType.LPStr)> ByVal lpAppName As String,
+      <MarshalAs(UnmanagedType.LPStr)> ByVal lpKeyName As String,
+      <MarshalAs(UnmanagedType.LPStr)> ByVal lpDefault As String,
+      <MarshalAs(UnmanagedType.LPStr)> ByVal lpReturnedString As StringBuilder,
+     ByVal nSize As Integer,
+     <MarshalAs(UnmanagedType.LPStr)> ByVal lpFileName As String) As Integer
+End Class
